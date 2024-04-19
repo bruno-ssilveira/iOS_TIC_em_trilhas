a59bdb19ae044b92825fe9d7e562d5757be8a761
ios-noite-20 <ios-noite-20@Administradors-MacBook-Pro.local>
2024-04-18 21:45:17 -0300

    First Commit

---
+++ ConhecendoXcode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ConhecendoXcode
+//
+//  Created by ios-noite-20 on 16/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
